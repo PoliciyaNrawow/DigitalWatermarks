3ed0fa82d75b4d3069b97279212027169555d362
Булат Якупов <yakupov.bulat@gmail.com>
2016-11-13 22:35:44 +0300

    added encode/decode functions. decode not finished

--- frame_manager.py
+++ frame_manager.py
@@ -1,21 +1,47 @@
 import cv2
 import numpy as np
+import wavelets as wl
 #import scipy as sp
 
-cap = cv2.VideoCapture("buy.mov")
 
-#if video.isOpened() == False:
-#    video.open()
+def encodeVideo(video):
+    
+    if video.isOpened() == False:
+        video.open()
+    
+    fourcc = cv2.VideoWriter_fourcc(*'X264')
+    out = cv2.VideoWriter('output.mpeg',fourcc, 25.0, (1280, 720))
 
-retval = True
+    while (video.isOpened()):
+        retval, frame = video.read()
+        if retval == True:
+            transformedFrame = wl.TransformImage(frame)
+            out.write(transformedFrame)
+        else:
+            break
 
-while (retval):
-    retval, frame = cap.read()
-    if retval == True:
-    #cv2.imshow('frame', frame)
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
+    video.release()
+    out.release()
 
-
-cap.release()
-cv2.destroyAllWindows()
+def decodeVideo(originalVideo, encodedVideo):
+    if originalVideo.isOpened() == False:
+        originalVideo.open()
+    
+    if encodedVideo.isOpened() == False:
+        encodedVideo.open()
+    
+    watermarkArray = []
+    
+    while (originalVideo.isOpened() && encodedVideo.isOpened()):
+        orgRetVal, orgFrame = originalVideo.read()
+        encRetVal, encFrame = encodedVideo.read()
+        
+        if orgRetVal == True && encRetVal == True:
+            watermarkArray.append(wl.RetrieveWat(encFrame, orgFrame))
+            if len(watermarkArray) == 25 :
+                res = np.array(watermarkArray[0])
+                for i in range(1, 24):
+                    res += np.array(watermarkArray[i])
+                res = res/25
+                res.astype(numpy.uint8)
+                
