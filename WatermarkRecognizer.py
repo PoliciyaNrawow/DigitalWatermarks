427acc52f30a2179e45de198313b684140076b35
Якупов Булат <yakupov.bulat@gmail.com>
2016-12-17 02:35:00 +0300

    changed recognition function and standard images

--- WatermarkRecognizer.py
+++ WatermarkRecognizer.py
@@ -6,7 +6,7 @@ def recognizeWatermark(watermark):
     image = Image.fromarray(watermark)
     image.load()
     
-    boxes = [(0, 0, 256, 256), (256, 0, 512, 256), (0, 256, 256, 512), (256, 256, 512, 512)]
+    boxes = [(0, 0, 64, 64), (64, 0, 128, 64), (0, 64, 64, 128), (64, 64, 128, 128)]
     
     images = [None] * 4
     
@@ -17,21 +17,23 @@ def recognizeWatermark(watermark):
     pics = [None] * 10
     
     for i in range(10):
-        pic = Image.open(str(i)+'.jpg')
+        pic = Image.open(str(i)+'re.jpg')
         pic = pic.convert('L')
         pix = pic.load()
         pics[i] = pix
+    
     
     pin_code = [None] * 4
 
     for itr, img in enumerate(images):
         norm = np.empty(10)
         for indx, pic in enumerate(pics):
-            arr = np.empty([256, 256])
-            for i in range(256):
-                for j in range(256):
+            arr = np.empty([64, 64])
+            for i in range(64):
+                for j in range(64):
                     arr[i][j] = img[i, j] - pic[i, j]
             norm[indx] = np.linalg.norm(arr)
         pin_code[itr] = np.argmin(norm)
+        print(norm)
 
     return pin_code
